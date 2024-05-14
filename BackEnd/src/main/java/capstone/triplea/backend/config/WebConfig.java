bac930c5ae35e1c917ae140b2696a8c36ff394d8
duse133 <js649578@naver.com>
2024-05-14 19:03:58 +0900

    Version Up

--- BackEnd/src/main/java/capstone/triplea/backend/config/WebConfig.java
+++ BackEnd/src/main/java/capstone/triplea/backend/config/WebConfig.java
@@ -14,6 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
      */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowCredentials(true).allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**").allowCredentials(true).allowedOrigins("http://52.62.34.185:3000");
     }
 }
